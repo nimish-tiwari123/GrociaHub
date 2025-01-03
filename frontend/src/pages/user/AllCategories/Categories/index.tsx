ecab784023c8851cdb21a65cb84f2175b4151a5a
nimish-tiwari123 <nimisht.6555@gmail.com>
2025-01-03 16:53:19 +0530

    removed button in category card

--- frontend/src/pages/user/AllCategories/Categories/index.tsx
+++ frontend/src/pages/user/AllCategories/Categories/index.tsx
@@ -1,54 +1,36 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { Button } from "../../../../components/common";
-import {
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-} from "../../../../assets/categories";
+import { Button, CategorySkeleton } from "../../../../components/common";
+
 import "./style.css";
+import { useViewUserCategoryQuery } from "../../../../api/userService";
 const Categories = () => {
-  const CategoriesImg = [
-    { name: "Fruits", img: image1, totalProducts: 20 },
-    { name: "Fruits", img: image2, totalProducts: 20 },
-    { name: "Fruits", img: image3, totalProducts: 20 },
-    { name: "Fruits", img: image4, totalProducts: 20 },
-    { name: "Fruits", img: image5, totalProducts: 20 },
-    { name: "Fruits", img: image6, totalProducts: 20 },
-    { name: "Fruits", img: image7, totalProducts: 20 },
-    { name: "Fruits", img: image3, totalProducts: 20 },
-    { name: "Fruits", img: image4, totalProducts: 20 },
-    { name: "Fruits", img: image5, totalProducts: 20 },
-    { name: "Fruits", img: image6, totalProducts: 20 },
-    { name: "Fruits", img: image7, totalProducts: 20 },
-    { name: "Fruits", img: image3, totalProducts: 20 },
-    { name: "Fruits", img: image2, totalProducts: 20 },
-    { name: "Fruits", img: image3, totalProducts: 20 },
-    { name: "Fruits", img: image4, totalProducts: 20 },
-    { name: "Fruits", img: image5, totalProducts: 20 },
-  ];
+  const { data, isLoading } = useViewUserCategoryQuery("");
+  type categoryType = {
+    image: string;
+    name: string;
+    totalProducts: string;
+  };
   return (
     <Container>
       <Row className="my-5">
-        {CategoriesImg.map((item, index) => (
+        {isLoading &&
+          Array.from({ length: 12 }).map((_, index) => (
+            <Col key={index} className="col-6" sm={4} md={3} lg={2}>
+              <CategorySkeleton />
+            </Col>
+          ))}
+        {data?.categories?.map((item: categoryType, index: number) => (
           <Col key={index} className="col-6" sm={4} md={3} lg={2}>
             <div className="rounded-2 p-3 mt-3 border">
               <img
-                src={item.img}
+                src={item.image}
                 alt="category"
-                className="w-75 m-auto d-block"
+                className="w-75 m-auto d-block category-admin-img"
               />
-              <h5 className="text-center">{item.name}</h5>
+              <h5 className="text-center fs-6">{item.name}</h5>
               <p className="text-center opacity-75 fs-7">
                 {item.totalProducts} Products
               </p>
-              <Button
-                btnLabel="View Products"
-                btnStyle="bg-custom-primary border-0 text-light px-3 fw-medium py-1 m-auto fs-7 rounded-pill"
-              />
             </div>
           </Col>
         ))}
