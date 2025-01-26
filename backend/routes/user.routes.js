5c53b08159c4601a2418b0f1a56a4a2593d2af81
Keshav Agrawal <keshavagrawal1123@gmail.com>
2025-01-26 10:59:58 +0530

    update route added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", userControllers.getUsers);
 router.post("/", userControllers.createUser);
+router.patch("/:id", userControllers.updateUser);
 router.post("/login", userControllers.loginUser);
 router.post("/forgot-password", userControllers.forgotPassword);
 router.post("/reset-password", userControllers.resetPassword);
